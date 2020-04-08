1ae58bdc0599a01983cca3cccd3067d2bd2596f8
Daniela <daniela.arroyo27@gmail.com>
2020-04-08 11:50:01 -0500

    step one

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = () => {
   return (
-    <>
-      <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projectmanagment">Project Management</Link></li>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
 
-          </ul>
-        </div>
-      </nav>
-    </>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/projectmanagement">Project Management</Link></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BaseLayout from './components/layout/BaseLayout';
@@ -14,7 +14,7 @@ ReactDOM.render(
     <BaseLayout>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route exact path="/projectmanagment" component={ProjectManagement} />
+        <Route exact path="/projectmanagement" component={ProjectManagement} />
       </Switch>
     </BaseLayout>
   </BrowserRouter>,
